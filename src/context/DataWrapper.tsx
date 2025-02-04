d877197a319386b5a47a54b79aa6135ed0e1c5c9
Saurabh Ahlawat <saurabha2014@email.iimcal.ac.in>
2025-02-04 12:39:43 -0500

    changes to socket2

--- src/context/DataWrapper.tsx
+++ src/context/DataWrapper.tsx
@@ -851,17 +851,12 @@ export default function DataWrapper({
   useEffect(() => {
     if (myId === "" || meetingIsLegit === false) return;
 
-    //This is a socket connection to handle live messages between participants\
-
-    let socket1url1 = "https://vitt-jarvis-node-production.up.railway.app/"
-    let socket1url2 = 'https://vitt-two-way-node.vercel.app/'
-    let socket1url3 = 'http://localhost:3002'
-    
-    let tempSocket = io(socket1url1);
+    //This is a socket connection to handle live messages between participants\    
+    let tempSocket = io("https://vitt-jarvis-node-production.up.railway.app/");
 
     //This is a socket connection with backend server to handle cues specific requests or other api requests
     let tempSocket2 = io(
-      "ws://34.47.233.254"
+      "wss://recruito.vitti.insure"
       //'http://localhost:5000'
     );
     // https://vitt-ai-request-broadcaster-production.up.railway.app
