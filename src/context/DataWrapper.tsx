1c1958a97d7de0def9579c14ca2e2208a8da1bfa
anshtiwari314 <anshtiwari314@gmail.com>
2025-02-15 01:55:15 +0530

    added some stun servers to iceServers key

--- src/context/DataWrapper.tsx
+++ src/context/DataWrapper.tsx
@@ -202,7 +202,11 @@ export default function DataWrapper({
     //secure: true,
     config: {
       iceServers: [
-        { url: 'stun:stun.l.google.com:19302' },
+        { urls: 'stun:stun.l.google.com:19302' },
+        {urls:'stun:stun1.l.google.com:19302'},
+        {urls:'stun:stun2.l.google.com:19302'},
+        {urls:'stun:stun3.l.google.com:19302'},
+        {urls:'stun:stun4.l.google.com:19302'}
       ]
     }
   }
